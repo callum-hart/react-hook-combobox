51717cfae22de42d830fb53de5d6e6e75193e780
Callum Hart <callum@callumhart.com>
2019-12-03 22:00:06 +0000

    Initial commit 🎉

---
+++ src/useCombobox.js
@@ -0,0 +1,217 @@
+import { useState, useEffect, useRef } from "react";
+
+const DEFAULT_ACTIVE_INDEX = -1;
+const KEY_ARROW_DOWN = "ArrowDown";
+const KEY_ARROW_UP = "ArrowUp";
+const KEY_PAGE_DOWN = "PageDown";
+const KEY_PAGE_UP = "PageUp";
+const KEY_ENTER = "Enter";
+const KEY_TAB = "Tab";
+const KEY_ESCAPE = "Escape";
+
+const useCombobox = ({ name, initialValue = "", optionToString, onChange }) => {
+  const [term, setTerm] = useState(initialValue);
+  const [activeIndex, setActiveIndex] = useState(DEFAULT_ACTIVE_INDEX);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const containerRef = useRef(null);
+  const labelRef = useRef(null);
+  const inputRef = useRef(null);
+  const listboxRef = useRef(null);
+
+  const labelId = `${name}-label`;
+  const inputId = `${name}-input`;
+  const listboxId = `${name}-listbox`;
+
+  const termHasFocus =
+    inputRef.current && inputRef.current === document.activeElement;
+
+  let optionsCount = 0;
+
+  useEffect(() => {
+    if (termHasFocus) {
+      document.addEventListener("keydown", handleDocumentKeydown);
+
+      return () =>
+        document.removeEventListener("keydown", handleDocumentKeydown);
+    }
+  });
+
+  useEffect(() => {
+    if (termHasFocus) {
+      document.addEventListener("click", handleDocumentClick);
+
+      return () => document.removeEventListener("click", handleDocumentClick);
+    }
+  });
+
+  const handleArrowDown = () => {
+    const newActiveIndex =
+      activeIndex + 1 >= optionsCount ? 0 : activeIndex + 1;
+
+    handleNewActiveIndex(newActiveIndex);
+  };
+
+  const handleArrowUp = () => {
+    const newActiveIndex =
+      activeIndex - 1 < 0 ? optionsCount - 1 : activeIndex - 1;
+
+    handleNewActiveIndex(newActiveIndex);
+  };
+
+  const handlePageDown = () => {
+    handleNewActiveIndex(optionsCount - 1);
+  };
+
+  const handlePageUp = () => {
+    handleNewActiveIndex(0);
+  };
+
+  const handleNewActiveIndex = newActiveIndex => {
+    if (
+      listboxRef.current &&
+      listboxRef.current.children &&
+      listboxRef.current.children[newActiveIndex]
+    ) {
+      listboxRef.current.children[newActiveIndex].scrollIntoView();
+      setActiveIndex(newActiveIndex);
+    }
+  };
+
+  const handleEnter = () => selectOption(activeIndex);
+
+  const handleTab = () => {
+    inputRef.current.blur();
+    setActiveIndex(DEFAULT_ACTIVE_INDEX);
+    setIsOpen(false);
+  };
+
+  const handleEscape = () => {
+    setTerm("");
+    setActiveIndex(DEFAULT_ACTIVE_INDEX);
+    setIsOpen(false);
+  };
+
+  const handleDocumentKeydown = ({ key }) => {
+    if (!isOpen && (key === KEY_ARROW_DOWN || key === KEY_ARROW_UP)) {
+      // user pressed esc and maintained focus
+      setIsOpen(true);
+    }
+
+    switch (key) {
+      case KEY_ARROW_DOWN:
+        return handleArrowDown();
+      case KEY_ARROW_UP:
+        return handleArrowUp();
+      case KEY_PAGE_DOWN:
+        return handlePageDown();
+      case KEY_PAGE_UP:
+        return handlePageUp();
+      case KEY_ENTER:
+        return handleEnter();
+      case KEY_TAB:
+        return handleTab();
+      case KEY_ESCAPE:
+        return handleEscape();
+      default:
+        return null;
+    }
+  };
+
+  const handleDocumentClick = ({ target }) => {
+    if (isOpen && !containerRef.current.contains(target)) {
+      inputRef.current.blur();
+      setActiveIndex(DEFAULT_ACTIVE_INDEX);
+      setIsOpen(false);
+    }
+  };
+
+  const selectOption = index => {
+    const displayName = optionToString(index);
+
+    if (displayName) {
+      setTerm(displayName);
+      onChange(displayName, index);
+      setIsOpen(false);
+    } // else user pressed enter without a term present
+  };
+
+  return {
+    primitives: {
+      container: {
+        ref: containerRef,
+        role: "combobox",
+        "aria-haspopup": "listbox",
+        "aria-owns": listboxId,
+        "aria-expanded": isOpen
+      },
+      label: {
+        ref: labelRef,
+        id: labelId,
+        htmlFor: inputId
+      },
+      input: {
+        ref: inputRef,
+        value: term,
+        id: inputId,
+        autoComplete: "off",
+        "aria-autocomplete": "list", // try 'both' / setSelectionRange
+        "aria-controls": listboxId,
+        ...(isOpen &&
+          activeIndex > DEFAULT_ACTIVE_INDEX && {
+            "aria-activedescendant": `${name}-option-${activeIndex}`
+          }),
+        // only apply aria-labelledby if consumer has a label
+        ...(labelRef.current && {
+          "aria-labelledby": labelId,
+        }),
+        onFocus: () => setIsOpen(true),
+        onChange: ({ target: { value } }) => {
+          setTerm(value);
+          setActiveIndex(DEFAULT_ACTIVE_INDEX);
+          setIsOpen(true);
+        }
+      },
+      listbox: {
+        ref: listboxRef,
+        id: listboxId,
+        role: "listbox",
+        ...(labelRef.current && {
+          "aria-labelledby": labelId,
+        }),
+      },
+      listboxOption: index => {
+        optionsCount++;
+
+        return {
+          id: `${name}-option-${index}`,
+          role: "option",
+          "aria-selected": index === activeIndex,
+          onClick: () => selectOption(index)
+        };
+      }
+    },
+    term,
+    activeIndex,
+    isOpen,
+    handleReset: () => {
+      setTerm("");
+      setActiveIndex(DEFAULT_ACTIVE_INDEX);
+      onChange("");
+    },
+    handleSearch: () => {
+      setTerm(term);
+      setActiveIndex(DEFAULT_ACTIVE_INDEX);
+      onChange(term);
+      setIsOpen(false);
+    }
+  };
+};
+
+export const Combobox = ({ children, ...otherProps }) => {
+  const comboboxGoodies = useCombobox({ ...otherProps });
+
+  return children({ ...comboboxGoodies });
+};
+
+export default useCombobox;
---
+++ stories/combobox.story.js
@@ -0,0 +1,73 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+
+import useCombobox from "../src/useCombobox";
+
+const givenOptions = [
+  "London",
+  "Paris",
+  "New York",
+  "Tokyo",
+  "Barcelona",
+  "Moscow",
+  "Chicago",
+  "Singapore",
+  "Dubai",
+  "San Francisco",
+  "Madrid",
+  "Amsterdam",
+  "Los Angeles",
+  "Rome",
+  "Boston",
+  "San Jose",
+  "Toronto",
+  "Washington DC",
+  "Zurich",
+  "Hong Kong",
+  "Beijing",
+  "Berlin",
+  "Sydney",
+  "Las Vegas",
+  "Frankfurt"
+];
+
+const Example = () => {
+  const { primitives, term, activeIndex, isOpen } = useCombobox({
+    name: "city",
+    optionToString: index => filteredOptions[index],
+    onChange: (value, index) => console.log(value, filteredOptions[index])
+  });
+  const filteredOptions = givenOptions.filter(option =>
+    option.startsWith(term)
+  );
+
+  return (
+    <div {...primitives.container}>
+      <label {...primitives.label}>City</label>
+
+      <input type="text" {...primitives.input} />
+
+      {isOpen &&
+        (filteredOptions.length ? (
+          <ul
+            {...primitives.listbox}
+            style={{ maxHeight: "200px", overflow: "auto" }}
+          >
+            {filteredOptions.map((option, index) => (
+              <li
+                {...primitives.listboxOption(index)}
+                key={index}
+                style={index === activeIndex ? { background: "#D8E2EB" } : {}}
+              >
+                {option}
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>No results</p>
+        ))}
+    </div>
+  );
+};
+
+storiesOf("useCombobox", module).add("default", () => <Example />);
