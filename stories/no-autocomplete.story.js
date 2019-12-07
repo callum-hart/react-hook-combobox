e60b6938e937ec11d6c9b6891c85d5a262b1a1bf
Callum Hart <callum@callumhart.com>
2019-12-07 19:31:43 +0000

    No autocomplete example

---
+++ stories/no-autocomplete.story.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+
+import useCombobox from "../src/useCombobox";
+
+const givenOptions = [
+  { id: "1", firstName: "Emma" },
+  { id: "2", firstName: "Olivia" },
+  { id: "3", firstName: "Ava" },
+  { id: "4", firstName: "Liam" },
+  { id: "5", firstName: "Noah" },
+  { id: "6", firstName: "William" }
+];
+
+const Example = () => {
+  const { primitives, activeIndex, isOpen } = useCombobox({
+    name: "recent-contacts",
+    optionToString: index => givenOptions[index].firstName,
+    onChange: (value, index) => console.log(value, givenOptions[index])
+  });
+
+  return (
+    <div {...primitives.container}>
+      <label {...primitives.label}>Contact</label>
+
+      <input type="text" {...primitives.input} />
+
+      {isOpen && (
+        <>
+          <p>Recently contacted:</p>
+          <ul {...primitives.listbox}>
+            {givenOptions.map((contact, index) => (
+              <li
+                {...primitives.listboxOption(index)}
+                key={contact.id}
+                style={index === activeIndex ? { background: "#D8E2EB" } : {}}
+              >
+                {contact.firstName}
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
+    </div>
+  );
+};
+
+storiesOf("useCombobox", module).add("No autocomplete", () => <Example />);
