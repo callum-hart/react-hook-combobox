f428d3fe21a410ed1717a40f2f61f1adb8403f63
Callum Hart <callum@callumhart.com>
2019-12-16 22:02:40 +0000

    Test initialValue, handleOpen, handleClear, ArrowDown + ArrowUp

--- src/useCombobox.test.js
+++ src/useCombobox.test.js
@@ -1,9 +1,11 @@
-import { renderHook } from "@testing-library/react-hooks";
+import React from "react";
+import { renderHook, act } from "@testing-library/react-hooks";
 
 import useCombobox from "./useCombobox";
 
+const givenName = "given-name";
+
 describe("primitives", () => {
-  const givenName = "given-name";
   const { result } = renderHook(() => useCombobox({ name: givenName }));
   const primitives = result.current.primitives;
 
@@ -69,5 +71,135 @@ describe("primitives", () => {
     expect(primitives.listboxOption(givenIndex)).toStrictEqual(
       expectedResponse
     );
+  });
+});
+
+describe("initialValue", () => {
+  const givenInitialValue = "given-initial-value";
+  const { result } = renderHook(() =>
+    useCombobox({ name: givenName, initialValue: givenInitialValue })
+  );
+
+  it("should should set term to the initialValue", () => {
+    expect(result.current.term).toStrictEqual(givenInitialValue);
+  });
+});
+
+describe("handleOpen", () => {
+  const focusMock = jest.fn();
+  jest
+    .spyOn(React, "useRef")
+    .mockReturnValue({ current: { focus: focusMock } });
+  const { result } = renderHook(() => useCombobox({ name: givenName }));
+
+  describe("when called", () => {
+    act(() => {
+      result.current.handleOpen();
+    });
+
+    it("should set isOpen to true", () => {
+      expect(result.current.isOpen).toBe(true);
+    });
+
+    it("should focus the input", () => {
+      expect(focusMock).toHaveBeenCalledWith();
+    });
+  });
+});
+
+describe("handleClear", () => {
+  const givenInitialValue = "given-initial-value";
+  const onChangeMock = jest.fn();
+  const { result } = renderHook(() =>
+    useCombobox({
+      name: givenName,
+      initialValue: givenInitialValue,
+      onChange: onChangeMock
+    })
+  );
+
+  describe("when called", () => {
+    act(() => {
+      result.current.handleClear();
+    });
+
+    it("should set term to an empty string", () => {
+      expect(result.current.term).toStrictEqual("");
+    });
+
+    it("should call onChange with an empty string", () => {
+      expect(onChangeMock).toHaveBeenCalledWith("");
+    });
+
+    it("should set isOpen to false", () => {
+      expect(result.current.isOpen).toBe(false);
+    });
+  });
+});
+
+describe("primitives.input.onKeyDown", () => {
+  const listboxRefChildrenMock = [
+    { scrollIntoView: jest.fn() },
+    { scrollIntoView: jest.fn() }
+  ];
+
+  jest.spyOn(React, "useRef").mockReturnValue({
+    current: {
+      children: listboxRefChildrenMock
+    }
+  });
+
+  describe("when called", () => {
+    describe("and key is ArrowDown", () => {
+      const { result } = renderHook(() => useCombobox({ name: givenName }));
+      const eventMock = { key: "ArrowDown" };
+      const expectedActiveIndex = 0;
+
+      act(() => {
+        result.current.primitives.listboxOption(0);
+        result.current.primitives.listboxOption(1);
+        result.current.primitives.input.onKeyDown(eventMock);
+      });
+
+      it("should set isOpen to true", () => {
+        expect(result.current.isOpen).toBe(true);
+      });
+
+      it("should set expected activeIndex", () => {
+        expect(result.current.activeIndex).toStrictEqual(expectedActiveIndex);
+      });
+
+      it("should call scrollIntoView", () => {
+        expect(
+          listboxRefChildrenMock[expectedActiveIndex].scrollIntoView
+        ).toHaveBeenCalledWith();
+      });
+    });
+
+    describe("and key is ArrowUp", () => {
+      const { result } = renderHook(() => useCombobox({ name: givenName }));
+      const eventMock = { key: "ArrowUp" };
+      const expectedActiveIndex = 1;
+
+      act(() => {
+        result.current.primitives.listboxOption(0);
+        result.current.primitives.listboxOption(1);
+        result.current.primitives.input.onKeyDown(eventMock);
+      });
+
+      it("should set isOpen to true", () => {
+        expect(result.current.isOpen).toBe(true);
+      });
+
+      it("should set expected activeIndex", () => {
+        expect(result.current.activeIndex).toStrictEqual(expectedActiveIndex);
+      });
+
+      it("should call scrollIntoView", () => {
+        expect(
+          listboxRefChildrenMock[expectedActiveIndex].scrollIntoView
+        ).toHaveBeenCalledWith();
+      });
+    });
   });
 });
