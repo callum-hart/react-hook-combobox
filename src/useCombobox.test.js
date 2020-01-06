6ba4c6a778a4ffb90cc21e585ede9a043e4048b8
Callum Hart <callum@callumhart.com>
2020-01-06 08:43:40 +0000

    Test conditional aria attributes

--- src/useCombobox.test.js
+++ src/useCombobox.test.js
@@ -6,34 +6,40 @@ import useCombobox from "./useCombobox";
 const givenName = "given-name";
 
 describe("primitives", () => {
-  const { result } = renderHook(() => useCombobox({ name: givenName }));
-  const primitives = result.current.primitives;
+  describe("container", () => {
+    const { result } = renderHook(() => useCombobox({ name: givenName }));
 
-  it("should return the expected container", () => {
-    const expectedResponse = {
-      ref: { current: null },
-      role: "combobox",
-      "aria-expanded": false,
-      "aria-haspopup": "listbox",
-      "aria-owns": `${givenName}-listbox`
-    };
+    it("should return the expected container", () => {
+      const expectedResponse = {
+        ref: { current: null },
+        role: "combobox",
+        "aria-expanded": false,
+        "aria-haspopup": "listbox",
+        "aria-owns": `${givenName}-listbox`
+      };
 
-    expect(primitives.container).toStrictEqual(expectedResponse);
+      expect(result.current.primitives.container).toStrictEqual(
+        expectedResponse
+      );
+    });
   });
 
-  it("should return the expected label", () => {
-    const expectedResponse = {
-      ref: { current: null },
-      id: `${givenName}-label`,
-      htmlFor: `${givenName}-input`
-    };
+  describe("label", () => {
+    const { result } = renderHook(() => useCombobox({ name: givenName }));
 
-    expect(primitives.label).toStrictEqual(expectedResponse);
+    it("should return the expected label", () => {
+      const expectedResponse = {
+        ref: { current: null },
+        id: `${givenName}-label`,
+        htmlFor: `${givenName}-input`
+      };
+
+      expect(result.current.primitives.label).toStrictEqual(expectedResponse);
+    });
   });
 
-  it("should return the expected input", () => {
+  describe("input", () => {
     const expectedResponse = {
-      ref: { current: null },
       value: "",
       id: `${givenName}-input`,
       autoComplete: "off",
@@ -44,33 +50,124 @@ describe("primitives", () => {
       onKeyDown: expect.any(Function)
     };
 
-    expect(primitives.input).toStrictEqual(expectedResponse);
+    describe("when there is an active option", () => {
+      const listboxRefChildrenMock = [{ scrollIntoView: jest.fn() }];
+
+      jest.spyOn(React, "useRef").mockReturnValue({
+        current: {
+          children: listboxRefChildrenMock
+        }
+      });
+
+      const { result } = renderHook(() => useCombobox({ name: givenName }));
+
+      act(() => {
+        result.current.primitives.listboxOption(0);
+        result.current.primitives.input.onKeyDown({ key: "ArrowDown" });
+      });
+
+      it("should return the expected input", () => {
+        expect(result.current.primitives.input).toStrictEqual({
+          ...expectedResponse,
+          ref: { current: { children: listboxRefChildrenMock } },
+          "aria-labelledby": `${givenName}-label`,
+          "aria-activedescendant": `${givenName}-option-${0}`
+        });
+      });
+    });
+
+    describe("when there is a label primitive", () => {
+      const useRefMock = "use-ref-mock";
+
+      jest.spyOn(React, "useRef").mockReturnValue({
+        current: useRefMock
+      });
+
+      const { result } = renderHook(() => useCombobox({ name: givenName }));
+
+      it("should return the expected input", () => {
+        expect(result.current.primitives.input).toStrictEqual({
+          ...expectedResponse,
+          ref: { current: useRefMock },
+          "aria-labelledby": `${givenName}-label`
+        });
+      });
+    });
+
+    describe("when there is NOT a label primitive", () => {
+      jest.spyOn(React, "useRef").mockReturnValue({
+        current: null
+      });
+
+      const { result } = renderHook(() => useCombobox({ name: givenName }));
+
+      it("should return the expected input", () => {
+        expect(result.current.primitives.input).toStrictEqual({
+          ...expectedResponse,
+          ref: { current: null }
+        });
+      });
+    });
   });
 
-  it("should return the expected listbox", () => {
+  describe("listbox", () => {
     const expectedResponse = {
-      ref: { current: null },
       id: `${givenName}-listbox`,
       role: "listbox",
       onMouseOut: expect.any(Function)
     };
 
-    expect(primitives.listbox).toStrictEqual(expectedResponse);
+    describe("when there is a label primitive", () => {
+      const useRefMock = "use-ref-mock";
+
+      jest.spyOn(React, "useRef").mockReturnValue({
+        current: useRefMock
+      });
+
+      const { result } = renderHook(() => useCombobox({ name: givenName }));
+
+      it("should return the expected listbox", () => {
+        expect(result.current.primitives.listbox).toStrictEqual({
+          ...expectedResponse,
+          ref: { current: useRefMock },
+          "aria-labelledby": `${givenName}-label`
+        });
+      });
+    });
+
+    describe("when there is NOT a label primitive", () => {
+      jest.spyOn(React, "useRef").mockReturnValue({
+        current: null
+      });
+
+      const { result } = renderHook(() => useCombobox({ name: givenName }));
+
+      it("should return the expected listbox", () => {
+        expect(result.current.primitives.listbox).toStrictEqual({
+          ...expectedResponse,
+          ref: { current: null }
+        });
+      });
+    });
   });
 
-  it("should return the expected listboxOption", () => {
-    const givenIndex = 0;
-    const expectedResponse = {
-      id: `${givenName}-option-${givenIndex}`,
-      role: "option",
-      "aria-selected": false,
-      onClick: expect.any(Function),
-      onMouseOver: expect.any(Function)
-    };
+  describe("listboxOption", () => {
+    const { result } = renderHook(() => useCombobox({ name: givenName }));
 
-    expect(primitives.listboxOption(givenIndex)).toStrictEqual(
-      expectedResponse
-    );
+    it("should return the expected listboxOption", () => {
+      const givenIndex = 0;
+      const expectedResponse = {
+        id: `${givenName}-option-${givenIndex}`,
+        role: "option",
+        "aria-selected": false,
+        onClick: expect.any(Function),
+        onMouseOver: expect.any(Function)
+      };
+
+      expect(result.current.primitives.listboxOption(givenIndex)).toStrictEqual(
+        expectedResponse
+      );
+    });
   });
 });
 
